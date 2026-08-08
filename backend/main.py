9bae044338184c20cb00b1e20d1b68d0deb9f75b
ikkaru <ikhsanr236@gmail.com>
2026-08-08 21:41:49 +0700

    refactor: changing pengiriman_route file to pengiriman for better understanding and changing branch name

--- backend/main.py
+++ backend/main.py
@@ -1,16 +1,17 @@
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
 
-from app.api.routers import armada, barang, karoseri, pengiriman_route
+from app.api.routers import armada, barang, karoseri
 from app.postgresql.database import init_db
 from app.postgresql.schema import pengiriman
+from backend.app.api.routers import pengiriman
 
 app = FastAPI(title="Volumetrix Backend API")
 
 app.include_router(armada.router)
 app.include_router(barang.router)
 app.include_router(karoseri.router)
-app.include_router(pengiriman_route.router)
+app.include_router(pengiriman.router)
 
 # On startup Event
 @app.on_event("startup")
